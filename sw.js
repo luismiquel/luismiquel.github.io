78e27f09a3828c4f6db1223eb20ed4d240533c79
luismiquel <luismiguelgarciadelasmorenas@gmail.com>
2025-12-13 12:11:08 +0100

    Fix: limpiar Service Worker y cachés antiguas

---
+++ sw.js
@@ -0,0 +1,24 @@
+﻿self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    try {
+      const keys = await caches.keys();
+      await Promise.all(keys.map((k) => caches.delete(k)));
+    } catch (e) {}
+
+    try {
+      await self.registration.unregister();
+    } catch (e) {}
+
+    // Toma control para que el “apagador” aplique ya
+    try {
+      await self.clients.claim();
+    } catch (e) {}
+  })());
+});
+
+// No intercepta nada: deja pasar toda la red
+self.addEventListener("fetch", () => {});
